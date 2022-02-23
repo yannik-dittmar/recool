cf148e3ed36284d564e56fbb710443e093b52a2a
Yannik Dittmar <email@yannik-dittmar.de>
2022-02-23 23:31:37 +0100

    Added network scanner with ping scan

--- ip_tools.py
+++ ip_tools.py
@@ -1,11 +1,37 @@
 import socket
 import ipaddress
+from tkinter.ttk import Style
+import nmap
+from pwn import log
+from colored import fg, bg, attr, stylize
+import recool
 
-def ParseIP(ip):
+def parse_ip(ip):
     try:
-        return ipaddress.ip_address(ip)
+        return str(ipaddress.ip_address(ip))
     except ValueError:
         return False
 
-def DefaultIP():
+def default_ip():
     return socket.gethostbyname(socket.gethostname())
+
+def paint_number(number):
+    if number == '0':
+        return stylize(number, recool.STYLE_FAILURE)
+    return stylize(number, recool.STYLE_SUCCESS)
+
+class NetworkScanner:
+    def __init__(self, ip, storage, speed=None):
+        self.nmap = nmap.PortScanner()
+        self.ip = ip
+        self.storage = storage
+        self.speed = speed
+        
+        if not self.speed:
+            self.speed = '-T4'
+        
+    def ping_scan_subnet(self, subnet):
+        iface = ipaddress.ip_interface(self.ip + '/' + subnet)
+        with log.progress(f'Ping-Scanning subnet {stylize(iface.network, recool.STYLE_HIGHLIGHT)}') as p:
+            result = self.nmap.scan(hosts=str(iface.network), arguments=f'-sn -n {self.speed}')
+            p.success(f'Found {paint_number(result["nmap"]["scanstats"]["uphosts"])} hosts.')
--- recool.py
+++ recool.py
@@ -1,42 +1,54 @@
 import argparse
+from multiprocessing.connection import wait
+from time import sleep
 import nmap
 import json
-from pwn import log
+from pwn import *
+from colored import fg, bg, attr, stylize
 import ip_tools
 
-def ParseArguments():
+STYLE_HIGHLIGHT = fg("orange_3") + attr("bold")
+STYLE_SUCCESS = fg("green") + attr("bold")
+STYLE_FAILURE = fg("red_3a") + attr("bold")
+
+def parse_arguments():
     parser = argparse.ArgumentParser(description='Reconstruct the hash for the I-Sec Keysafe.')
-    parser.add_argument('-i', '--ip', dest='ip', metavar='IP', type=str, default=ip_tools.DefaultIP(),
+    parser.add_argument('-i', '--ip', dest='ip', metavar='IP', type=str, default=ip_tools.default_ip(),
                         help='Your local IP address.')
     parser.add_argument('-s', '--storage-file', dest='storage', metavar='STORAGE_FILE', type=str, default=None,
                         help='The file where information about the network will be stored or loaded from.')
 
     args = parser.parse_args()
 
-    if not ip_tools.ParseIP(args.ip):
+    if not ip_tools.parse_ip(args.ip):
         log.error(f'The IP "{args.ip}" address you provided is not valid.')
-    args.ip = ip_tools.ParseIP(args.ip)
+    args.ip = ip_tools.parse_ip(args.ip)
 
     if not args.storage:
         args.storage = f'{args.ip}.json'
 
     return args
 
-def PrintBanner():
+def print_banner(args):
     print("""
 
- /$$$$$$$                                          /$$
-| $$__  $$                                        | $$
-| $$  \ $$  /$$$$$$   /$$$$$$$  /$$$$$$   /$$$$$$ | $$
-| $$$$$$$/ /$$__  $$ /$$_____/ /$$__  $$ /$$__  $$| $$
-| $$__  $$| $$$$$$$$| $$      | $$  \ $$| $$  \ $$| $$
-| $$  \ $$| $$_____/| $$      | $$  | $$| $$  | $$| $$
-| $$  | $$|  $$$$$$$|  $$$$$$$|  $$$$$$/|  $$$$$$/| $$
-|__/  |__/ \_______/ \_______/ \______/  \______/ |__/
-                                                      
-""")
+        /$$$$$$$                                          /$$
+        | $$__  $$                                        | $$
+        | $$  \ $$  /$$$$$$   /$$$$$$$  /$$$$$$   /$$$$$$ | $$
+        | $$$$$$$/ /$$__  $$ /$$_____/ /$$__  $$ /$$__  $$| $$
+        | $$__  $$| $$$$$$$$| $$      | $$  \ $$| $$  \ $$| $$
+        | $$  \ $$| $$_____/| $$      | $$  | $$| $$  | $$| $$
+        | $$  | $$|  $$$$$$$|  $$$$$$$|  $$$$$$/|  $$$$$$/| $$
+        |__/  |__/ \_______/ \_______/ \______/  \______/ |__/
+                                                            
+        """)
+    
+    log.info(f'Selected IP for network scanning: {stylize(args.ip, STYLE_HIGHLIGHT)}')
+    log.info(f'Selected storage file: {stylize(args.storage, STYLE_HIGHLIGHT)}')
 
 if __name__ == '__main__':
-    ParseArguments()
-    PrintBanner()
+    args = parse_arguments()
+    print_banner(args)
+    ns = ip_tools.NetworkScanner(args.ip, {})
+    ns.ping_scan_subnet('24')
     
