aae8262caa0d65bad6f19c00555584625dc0901f
Yannik Dittmar <email@yannik-dittmar.de>
2022-03-08 17:45:21 +0100

    shifting to use nplan and only execute scans

--- ip_tools.py
+++ ip_tools.py
@@ -1,9 +1,10 @@
-from imp import is_builtin
+from http import server
 import socket
 import ipaddress
 from telnetlib import NOP
-from typing import List, Set
+from typing import Dict, List, Set
 import nmap
+import os
 import json
 from json import JSONEncoder
 import logging as log
@@ -18,7 +19,7 @@ def parse_ip(ip):
 
 def default_ip():
     #return socket.gethostbyname(socket.gethostname())
-    return "192.168.188.10"
+    return "172.22.3.170"
 
 def keys_exists(element, *keys):
     '''
@@ -39,20 +40,61 @@ def keys_exists(element, *keys):
 
 class NetworkDevice():
     done_ping_scan: bool
-    done_scan: bool
+    done_full_scan: bool
     is_up: bool
     name: str
-    ipv4: Set[str]
-    ipv6: Set[str]
+    ip: str
+    services: Dict[str, str]
 
     def __init__(self, **kv):
         self.done_ping_scan = False
-        self.done_scan = False
+        self.done_full_scan = False
         self.is_up = False
         self.name = ""
-        self.ipv4 = set()
-        self.ipv6 = set()
+        self.ip = ""
+        self.services = {}
         self.__dict__.update(kv)
+
+    def __str__(self) -> str:
+        string = ""
+
+        # Name and IP
+        if self.name:
+            string += f'==={stylize(self.name, recool.STYLE_HIGHLIGHT)}===\n'
+        else:
+            string += f'==={stylize(self.ip, recool.STYLE_HIGHLIGHT)}===\n'
+        string += f'{stylize("IP:", recool.STYLE_HIGHLIGHT)} {self.ip}\n'
+
+        # Services
+        print_services = False
+        ports = self.services.keys()
+        for port in sorted(ports):
+            portInfo = self.services[port]
+            if portInfo["state"] == "open":
+                print_services = True
+                break
+
+        if print_services:
+            string += f'Open {stylize("TCP", recool.STYLE_HIGHLIGHT)}-Ports:\n'
+            for port in sorted(ports):
+                portInfo = self.services[port]
+                if portInfo["state"] != "open":
+                    continue
+                
+                portName = portInfo["name"]
+                portProd = portInfo["product"]
+                portVer = portInfo["version"]
+
+                if portName == "":
+                    string += f' - {stylize(port, recool.STYLE_HIGHLIGHT)}\n'
+                elif portProd == "":
+                    string += f' - {stylize(port, recool.STYLE_HIGHLIGHT)} ({portName})\n'
+                elif portVer == "":
+                    string += f' - {stylize(port, recool.STYLE_HIGHLIGHT)} ({portName} - {portProd})\n'
+                else:
+                    string += f' - {stylize(port, recool.STYLE_HIGHLIGHT)} ({portName} - {portProd}, {portVer})\n'
+
+        return string
 
 class NetworkEncoder(JSONEncoder):
     def default(self, o):
@@ -67,29 +109,37 @@ class NetworkScanner:
         self.args = args
         self.devices: List[NetworkDevice] = devices
         self.spinner = spinner
+
+    def scan(self, hosts: List[str], args: str):
+        os.system(f'sudo nmap -oX ./{self.args.storage}/scan.xml {args} {self.args.speed} {" ".join(hosts)} > /dev/null')
+        with open(f'./{self.args.storage}/scan.xml',mode='r') as scan_file:
+            result = self.nmap.analyse_nmap_xml_scan(nmap_xml_output=scan_file.read())
+
+        return result["scan"]
     
-    def find_by_address(self, address):
+    def find_by_ip(self, ip, create=True):
         for d in self.devices:
-            if address in d.ipv4:
+            if ip == d.ip:
                 return d
 
-        device = NetworkDevice()
-        self.devices.append(device)
-        return device
+        if create:
+            device = NetworkDevice(ip=ip)
+            self.devices.append(device)
+            return device
+        return None
 
-
-    def parse_device_data(self, address, data):
-        device: NetworkDevice = self.find_by_address(address)
+    def parse_device_data(self, ip, data):
+        device: NetworkDevice = self.find_by_ip(ip)
 
         # Hostname
         if (keys_exists(data, 'hostnames', 0, 'name') and 
                 data['hostnames'][0]['name']):
             device.name = data['hostnames'][0]['name']
         
-        # IPv6
-        if keys_exists(data, 'addresses', 'ipv6'):
-            device.ipv6.add(data['addresses']['ipv6'])
-        
+        if (keys_exists(data, 'tcp')):
+            for port, info in data['tcp'].items():
+                device.services[port] = info
+
         return device
 
     def ping_scan_subnet(self, subnet: str):
@@ -99,8 +149,7 @@ class NetworkScanner:
         # Collect hosts
         hosts = []
         for host in iface.network.hosts():
-            device: NetworkDevice = self.find_by_address(str(host))
-            device.ipv4.add(str(host))
+            device: NetworkDevice = self.find_by_ip(str(host))
             if not device.done_ping_scan and not device.is_up:
                 hosts.append(str(host))
         
@@ -108,25 +157,36 @@ class NetworkScanner:
             return
 
         # Perform scan and collect data
-        result = self.nmap.scan(hosts=' '.join(hosts), arguments=f'-sn -n {self.args.speed}')["scan"]
+        result = self.scan(hosts, '-sn -n')
         for ip, data in result.items():
             device = self.parse_device_data(ip, data)
             device.is_up = True
+            self.spinner.write(str(device))
         
         # Update done_ping_scan
         for host in iface.network.hosts():
-            device = self.find_by_address(str(host))
+            device = self.find_by_ip(str(host))
             device.done_ping_scan = True
 
-    def full_scan_up(self):
-        for device in self.devices:
-            if not device.is_up or device.done_scan:
+        #self.spinner.write(json.dumps(self.devices, cls=NetworkEncoder))
+
+    def full_scan_up(self, devices=None):
+        if not devices:
+            devices = self.devices
+
+        for device in devices:
+            if not device.is_up or device.done_full_scan:
                 continue
         
-            self.spinner.text = f'Performing full-scan for: {stylize(next(iter(device.ipv4)), recool.STYLE_HIGHLIGHT)}'
-            result = self.nmap.scan(hosts=next(iter(device.ipv4)), arguments=f'-A -p- {self.args.speed}')["scan"]
+            self.spinner.text = f'Performing full-scan for: {stylize(device.ip, recool.STYLE_HIGHLIGHT)}'
+            result = self.scan([device], '-A -p- -sV')
             for ip, data in result.items():
                 device = self.parse_device_data(ip, data)
-                device.done_scan = True
+                device.done_full_scan = True
+                self.spinner.write(str(device))
 
-        self.spinner.write(json.dumps(self.devices, cls=NetworkEncoder))
+            self.spinner.write(json.dumps(self.devices, cls=NetworkEncoder))
+
+    def test(self):
+        result = self.nmap.scan(hosts="10.129.0.2", arguments=f'{self.args.speed}')["scan"]
+        #self.spinner.write(self.nmap.)
--- recool.py
+++ recool.py
@@ -1,11 +1,11 @@
 import argparse
 from multiprocessing.connection import wait
 from time import sleep
-import nmap
-import json
+from pathlib import Path
 from yaspin import yaspin
 from colored import fg, bg, attr, stylize
 import logging as log
+from shutil import which
 import ip_tools
 
 STYLE_HIGHLIGHT = fg("orange_3") + attr("bold")
@@ -16,9 +16,9 @@ def parse_arguments():
     parser = argparse.ArgumentParser(description='Reconstruct the hash for the I-Sec Keysafe.')
     parser.add_argument('-i', '--ip', dest='ip', metavar='IP', type=str, default=ip_tools.default_ip(),
                         help='Your local IP address.')
-    parser.add_argument('-s', '--storage-file', dest='storage', metavar='STORAGE_FILE', type=str, default=None,
-                        help='The file where information about the network will be stored or loaded from.')
-    parser.add_argument('--speed', dest='speed', metavar='SPEED', type=str, default='-T5',
+    parser.add_argument('-s', '--storage-folder', dest='storage', metavar='STORAGE_FILE', type=str, default="recool",
+                        help='The folder where information about the network will be stored or loaded from.')
+    parser.add_argument('--speed', dest='speed', metavar='SPEED', type=str, default='-T4',
                         help='An nmap speed argument. Default: T4')
 
     args = parser.parse_args()
@@ -27,8 +27,7 @@ def parse_arguments():
         log.error(f'The IP "{args.ip}" address you provided is not valid.')
     args.ip = ip_tools.parse_ip(args.ip)
 
-    if not args.storage:
-        args.storage = f'{args.ip.replace(".", "-")}.json'
+    Path(args.storage).mkdir(parents=True, exist_ok=True)
 
     return args
 
@@ -50,7 +49,7 @@ def print_banner(args):
         """)
     
     log.info(f'Selected IP for network scanning: {stylize(args.ip, STYLE_HIGHLIGHT)}')
-    log.info(f'Selected storage file: {stylize(args.storage, STYLE_HIGHLIGHT)}')
+    log.info(f'Selected storage folder: {stylize(args.storage, STYLE_HIGHLIGHT)}')
     log.info(f'Selected speed argument: {stylize(args.speed, STYLE_HIGHLIGHT)}')
     log.info('')
 
@@ -61,6 +60,7 @@ if __name__ == '__main__':
     
     with yaspin(text="Initializing scan", color="yellow") as spinner:
         ns = ip_tools.NetworkScanner(args, [], spinner)
+        #ns.test()
         ns.ping_scan_subnet('24')
-        ns.full_scan_up()
+        #ns.full_scan_up()
     
